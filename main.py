d554e4af98f2f1592257b753aaedb1a41dfb4833
Yucheng Ding <yucheng.ding@connect.ust.hk>
2024-04-29 12:05:19 +0800

    Modify masks

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from ads1x15 import ADS1115
 
 class MyMCU:
     row_masks = [
-    [16, 1, 18, 3, 20, 5, 22, 7, 24, 9, 26, 11, 28, 13, 30, 15, 14, 31, 12, 29, 10, 27, 8, 25, 6, 23, 4, 21, 2, 19, 0, 17], # First pin in U1
+    [1, 16, 9, 24, 5, 20, 13, 28, 3, 18, 11, 26, 7, 22, 15, 30, 14, 31, 6, 23, 10, 27, 2, 19, 12, 29, 4, 21, 8, 25, 0, 17], # First pin in U1
     [1, 16, 3, 18, 5, 20, 7, 22, 9, 24, 11, 26, 13, 28, 15, 30, 31, 14, 29, 12, 27, 10, 25, 8, 23, 6, 21, 4, 19, 2, 17, 0]  # First pin in U3
     ]
     col_masks = [
-    [0, 17, 2, 19, 4, 21, 6, 23, 8, 25, 10, 27, 12, 29, 14, 31, 30, 15, 28, 13, 26, 11, 24, 9, 22, 7, 20, 5, 18, 3, 16, 1], # First pin in U2
+    [0, 17, 8, 25, 4, 21, 12, 29, 2, 19, 10, 27, 6, 23, 14, 31, 15, 30, 7, 22, 11, 26, 3, 18, 13, 28, 5, 20, 9, 24, 1, 16], # First pin in U2
     [17, 0, 19, 2, 21, 4, 23, 6, 25, 8, 27, 10, 29, 12, 31, 14, 15, 30, 13, 28, 11, 26, 9, 24, 7, 22, 5, 20, 3, 18, 1, 16]  # First pin in U4
     ]
-
     def __init__(self, row_type, col_type):
         # Initialize Multiplexers
         self.init_pins()
@@ -21,7 +20,7 @@ class MyMCU:
         
         # Initialize ADC
         adc_i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
-        self.adc = ADS1115(adc_i2c, 0x48, 1)
+        self.adc = ADS1115(adc_i2c, 0x48, 0)
         
         self.mode = machine.Pin(16, machine.Pin.OUT) # 0: Low Freq; 1: High Freq
         self.mode.value(0)
@@ -43,7 +42,7 @@ class MyMCU:
 
     def show_adc_reading(self):
         voltage = self.adc.raw_to_v(self.adc.read(channel1 = 1))
-        print("Voltage:", voltage, "V")
+        print(voltage)
 
     @staticmethod
     def number_to_binary_array(index):
@@ -73,7 +72,20 @@ class MyMCU:
         self.dac.write(int(255 * voltage / 3.3))
         
 row_type = 0
-col_type = 1
+col_type = 0
 my_mcu = MyMCU(row_type, col_type)
+my_mcu.select(16,16)
+time.sleep(10000)
+"""
+my_mcu.set_dac(0.5)
 my_mcu.scan()
 print(my_mcu.image)
+filename = 'data.csv'
+
+# 打开文件，准备写入
+with open(filename, 'w') as file:
+    for row in my_mcu.image:
+        # 将每一行数据转换为逗号分割的字符串
+        row_str = ','.join(str(item) for item in row)
+        # 写入一行数据，并添加换行符
+        file.write(row_str + '\n')"""
