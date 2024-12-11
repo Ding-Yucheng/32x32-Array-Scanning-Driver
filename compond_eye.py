ebba00b04dbade8e0371a219dc6f4863f021c6cd
Yucheng Ding <yucheng.ding@connect.ust.hk>
2024-12-11 13:45:51 +0800

    Esp32 optimization

--- compond_eye.py
+++ compond_eye.py
@@ -2,13 +2,41 @@ from machine import Pin, DAC, I2C, UART
 from ads1x15 import ADS1115
 import time
 
-# 全局变量用于存储传感器数据
-sensor_data = [[0 for _ in range(32)] for _ in range(32)]
+
+############ Setting ###############
+freq = 0 # 0 for low-freq, 1 for high-freq
+scan_interval = 10 # ms
+discard_sampling = 5
+sampling_number = 2
+
+adc_gain = 1
+'''
+    0: 6.144 V,  2/3x
+    1: 4.096 V,  1x
+    2: 2.048 V,  2x
+    3: 1.024 V,  4x
+    4: 0.512 V,  8x
+    5: 0.256 V,  16x
+'''
+adc_speed = 2
+'''
+    0: 8 SPS
+    1: 16 SPS
+    2: 32 SPS
+    3: 64 SPS
+    4: 128 SPS
+    5: 250 SPS
+    6: 475 SPS
+    7: 860 SPS
+'''
+####################################
+
+
 col_mask = [19, 2, 17, 0, 23, 6, 21, 4, 27, 10, 25, 8, 31, 14, 29, 12, 13, 28, 15, 30, 9, 24, 11, 26, 5, 20, 7, 22, 1, 16, 3, 18]
 row_mask = [3, 18, 1, 16, 7, 22, 5, 20, 11, 26, 9, 24, 15, 30, 13, 28, 29, 12, 31, 14, 25, 8, 27, 10, 21, 4, 23, 6, 17, 0, 19, 2]
-
-# 初始化串口，根据实际连接的串口引脚和需求调整参数
+#########UART INITIALIZING##########
 uart = UART(1, baudrate=115200, bits=8, parity=None, stop=1)
+####################################
 
 ##########MUX INITIALIZING##########
 Pin_R0 = Pin(12, mode=Pin.OUT, value=0)  # rows
@@ -36,13 +64,14 @@ Pin_sda = Pin(21, Pin.OUT)
 Pin_scl = Pin(22, Pin.OUT)
 i2c = I2C(1, scl=Pin_scl, sda=Pin_sda)
 
-adc = ADS1115(i2c, 0x48, 1)
-adc.set_conv(3, 1, None)
+adc = ADS1115(i2c, 0x48, adc_gain)
+adc.set_conv(adc_speed, 1, None)
 ####################################
 
 Pin_HL_Switch = Pin(16, mode=Pin.OUT)  # initialize pin controlling HL switch
-Pin_HL_Switch.value(0)  # 0 for low-freq, 1 for high-freq
+Pin_HL_Switch.value(freq) # 0 for low-freq, 1 for high-freq
 
+sensor_data = [[0 for _ in range(32)] for _ in range(32)]
 
 def select(pins, index):
     for i in range(5):
@@ -56,17 +85,16 @@ def scan():
         select(R_Pins, row_mask[i])
         for j in range(32):
             select(C_Pins, col_mask[j])
-            #time.sleep(0.01)#stablize, this number could be adjusted
-            for rep in range(20):
+            time.sleep_ms(scan_interval)
+            for rep in range(discard_sampling):
                 adc.read_rev()
-            value = adc.read_rev()
-            sensor_data[i][j] = value
-            # print(value)
-            # print(i, j, adc.raw_to_v(value))
+            value = 0
+            for rep in range(sampling_number):
+                value += adc.read_rev()
+            sensor_data[i][j] = int(value / sampling_number)
 
     dac.write(0)
     print(f"Frame time: {time.time() - t} seconds")
-    return sensor_data
 
 
 def list_to_str(data):
@@ -78,26 +106,25 @@ def list_to_str(data):
     sarr += 'end'
     return sarr
 
-while True:
+while not freq:
     if uart.any():
         time.sleep(0.01)
         command = uart.readline().decode('utf-8').strip()
         print("Serial Received: ", command)
         if command == "scan":
             print("Scanning...")
-            scanned_data = scan()
-            data_to_send = list_to_str(scanned_data).encode('utf-8')
+            data_to_send = list_to_str(sensor_data).encode('utf-8')
             try:
                 uart.write(data_to_send)
                 print("data sent!")
                 #print(data_to_send)
             except Exception as e:
                 print("Serial Error: ", e)
-'''
-dac.write(0)
-select(R_Pins, col_mask[12])
-select(C_Pins, col_mask[20])
-while True:
+if freq:
+    dac.write(0)
+    select(R_Pins, col_mask[12])
+    select(C_Pins, col_mask[20])
+
+while freq:
     print(adc.read_rev())
     time.sleep(0.1)
-'''
